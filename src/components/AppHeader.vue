368e08f90c79898f126c3b89ee8df95b947b9753
lewis <versus7063@gmail.com>
2025-07-17 18:15:23 +0800

    refactor: AppHeader組件改用setup語法糖

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,28 +1,21 @@
 <template>
 	<header class="w-100% flex-y-center bg-white h-60 shadow-primary">
-			<div class="font-bold text-24 ml-10 color-primary">TrackTrade</div>
-			<div @click.prevent="$emit('update:showMenu', true)" class="p-2 ml-auto mr-10">
-				<van-icon name="wap-nav" size="24" />
-			</div>
+		<div class="font-bold text-24 ml-10 color-primary">TrackTrade</div>
+		<div @click.prevent="$emit('update:showMenu', true)" class="p-2 ml-auto mr-10">
+			<van-icon name="wap-nav" size="24" />
+		</div>
 	</header>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
 import VanIcon from 'vant/lib/icon'
 import 'vant/lib/icon/style'
 
-export default defineComponent({
-	name: 'AppHeader',
-	components: {
-		VanIcon,
-	},
-	props: {
-		showMenu: {
-			type: Boolean,
-			required: true,
-		},
-	},
-	emits: ['update:showMenu'],
-})
+const props = defineProps<{
+	showMenu: boolean
+}>()
+
+const emit = defineEmits<{
+	(e: 'update:showMenu', value: boolean): void
+}>()
 </script>
