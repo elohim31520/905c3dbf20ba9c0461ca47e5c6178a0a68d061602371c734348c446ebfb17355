b0996e91e97dd50aea87c31cdf6fb85540cfcd4d
lewis <versus7063@gmail.com>
2025-07-23 14:46:52 +0800

    update: 修改波動區塊的字體顏色

--- src/components/VolatileAsset.vue
+++ src/components/VolatileAsset.vue
@@ -19,18 +19,26 @@ onMounted(async () => {
 			<div>
 				<h3 class="font-bold text-green-600 mb-2">強勢股</h3>
 				<ul>
-					<li v-for="stock in winners" :key="stock.id" class="flex justify-between items-center py-2 border-b px-10 py-20 shadow-primary">
+					<li
+						v-for="stock in winners"
+						:key="stock.id"
+						class="flex justify-between items-center py-2 px-10 py-20 shadow-primary"
+					>
 						<span class="color-gray-600">{{ stock.company }}</span>
 						<span class="text-green-600">{{ stock.dayChg }}%</span>
 					</li>
 				</ul>
 			</div>
 			<div>
-				<h3 class="font-bold text-red-600 mb-2">弱勢股</h3>
+				<h3 class="font-bold text-red-500 mb-2">弱勢股</h3>
 				<ul>
-					<li v-for="stock in losers" :key="stock.id" class="flex justify-between items-center py-2 border-b px-10 py-20 shadow-primary">
+					<li
+						v-for="stock in losers"
+						:key="stock.id"
+						class="flex justify-between items-center py-2 px-10 py-20 shadow-primary"
+					>
 						<span class="color-gray-600">{{ stock.company }}</span>
-						<span class="text-red-600">{{ stock.dayChg }}%</span>
+						<span class="text-red-500">{{ stock.dayChg }}%</span>
 					</li>
 				</ul>
 			</div>
