d45783c42b8f0bddee943273961ae0ff8b03afc3
lewis <versus7063@gmail.com>
2025-08-07 16:10:18 +0800

    update: 優化Records頁面的樣式

--- src/views/Records.vue
+++ src/views/Records.vue
@@ -1,26 +1,47 @@
 <template>
-	<div class="bg-white p-10 rounded-lg shadow-primary">
-		<h1 class="text-20 font-bold mb-4 color-#434343">交易紀錄</h1>
-		<van-list v-model:loading="loading" :finished="finished" finished-text="沒有更多了" @load="onLoad">
-			<div v-for="item in list" :key="item.id" class="bb-gray-1 pb-10 mb-4">
-				<div>
-					<span class="font-500">股票代號:</span> <span class="color-#565656">{{ item.stock_id }}</span>
+	<div class="bg-gray-50">
+		<div class="mx-10 bg-white p-10 rounded-15 shadow-lg">
+			<h1 class="text-[24px] font-bold mb-6 text-gray-800">交易紀錄</h1>
+			<van-list v-model:loading="loading" :finished="finished" finished-text="沒有更多了" @load="onLoad">
+				<div
+					v-for="item in list"
+					:key="item.id"
+					class="mb-15 border border-gray-200 rounded-lg p-4 transition-shadow duration-300 hover:shadow-md"
+				>
+					<div class="flex justify-between items-start mb-3">
+						<div>
+							<span class="text-[14px] text-gray-500">股票代號</span>
+							<p class="font-semibold text-[18px] text-gray-900">{{ item.stock_id }}</p>
+						</div>
+						<span
+							:class="[
+								'px-3 py-1 rounded-full text-[12px] font-semibold tracking-wide',
+								item.transaction_type === 'buy'
+									? 'bg-green-100 text-green-800'
+									: 'bg-red-100 text-red-800',
+							]"
+						>
+							{{ item.transaction_type === 'buy' ? '買入' : '賣出' }}
+						</span>
+					</div>
+
+					<div class="grid grid-cols-2 gap-4 text-[14px] mb-3">
+						<div>
+							<span class="text-gray-500">數量</span>
+							<p class="text-gray-800">{{ item.quantity }}</p>
+						</div>
+						<div>
+							<span class="text-gray-500">價格</span>
+							<p class="text-gray-800">{{ item.price }}</p>
+						</div>
+					</div>
+
+					<div class="text-right text-[12px] text-gray-400">
+						{{ item.transaction_date }}
+					</div>
 				</div>
-				<div>
-					<span class="font-500">類型:</span>
-					<span class="color-#565656">{{ item.transaction_type === 'buy' ? '買入' : '賣出' }}</span>
-				</div>
-				<div>
-					<span class="font-500">數量:</span> <span class="color-#565656">{{ item.quantity }}</span>
-				</div>
-				<div>
-					<span class="font-500">價格:</span> <span class="color-#565656">{{ item.price }}</span>
-				</div>
-				<div>
-					<span class="font-500">日期:</span> <span class="color-#565656">{{ item.transaction_date }}</span>
-				</div>
-			</div>
-		</van-list>
+			</van-list>
+		</div>
 	</div>
 </template>
 
