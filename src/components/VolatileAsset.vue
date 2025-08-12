edaec2aa688385d7e02f6cf11e794c697a03c617
lewis <versus7063@gmail.com>
2025-08-12 14:32:19 +0800

    refactor: 優化volitileAssets組件，使用vant-tab

--- src/components/VolatileAsset.vue
+++ src/components/VolatileAsset.vue
@@ -1,36 +1,24 @@
-<script lang="ts" setup>
-import { ref, onMounted } from 'vue'
-import { marketApi } from '../api/market'
-
-const winners = ref<any[]>([])
-const losers = ref<any[]>([])
-
-onMounted(async () => {
-	const [winnersRes, losersRes] = await Promise.all([marketApi.getStockWinners(), marketApi.getStockLosers()])
-
-	winners.value = winnersRes.data.slice(0, 5)
-	losers.value = losersRes.data.slice(0, 5)
-})
-</script>
-
 <template>
-	<div class="p-4 bg-white rounded-lg shadow-primary">
-		<div class="grid grid-cols-1 gap-4 px-5">
-			<div>
-				<h3 class="font-bold text-green-600 mb-2">強勢股</h3>
-				<ul>
-					<li
-						v-for="stock in winners"
-						:key="stock.id"
-						class="flex justify-between items-center py-2 px-10 py-20 shadow-primary"
-					>
-						<span class="color-gray-600">{{ stock.company }}</span>
-						<span class="text-green-600">{{ stock.dayChg }}%</span>
-					</li>
-				</ul>
+	<van-tabs v-model:active="activeTab" type="card" class="p-2" color="#f472b6">
+		<van-tab title="強勢股">
+			<div class="grid grid-cols-1 gap-4 px-5 mt-8">
+				<div>
+					<ul>
+						<li
+							v-for="stock in winners"
+							:key="stock.id"
+							class="flex justify-between items-center py-2 px-10 py-20 shadow-primary"
+						>
+							<span class="color-gray-600">{{ stock.company }}</span>
+							<span class="text-green-600">{{ stock.dayChg }}%</span>
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div>
-				<h3 class="font-bold text-red-500 mb-2">弱勢股</h3>
+		</van-tab>
+
+		<van-tab title="弱勢股">
+			<div class="grid grid-cols-1 gap-4 px-5 my-10">
 				<ul>
 					<li
 						v-for="stock in losers"
@@ -42,6 +30,22 @@ onMounted(async () => {
 					</li>
 				</ul>
 			</div>
-		</div>
-	</div>
+		</van-tab>
+	</van-tabs>
 </template>
+
+<script lang="ts" setup>
+	import { ref, onMounted } from 'vue'
+	import { marketApi } from '../api/market'
+
+	const winners = ref<any[]>([])
+	const losers = ref<any[]>([])
+	const activeTab = ref(0)
+
+	onMounted(async () => {
+		const [winnersRes, losersRes] = await Promise.all([marketApi.getStockWinners(), marketApi.getStockLosers()])
+
+		winners.value = winnersRes.data.slice(0, 5)
+		losers.value = losersRes.data.slice(0, 5)
+	})
+</script>
