67ffa1d7489e676b573b2f38f94e5b8be498804b
lewis <versus7063@gmail.com>
2025-08-12 11:19:36 +0800

    refactor: 統一引入lodash-es

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
+import { lodashImports } from './config/lodash-imports.js'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
-		vueDevTools(),
+		// vueDevTools(),
 		UnoCSS(),
 		AutoImport({
 			resolvers: [VantResolver()],
-			imports: ['vue', 'vue-router'],
+			imports: [
+				'vue',
+				'vue-router',
+				{
+					from: 'vant',
+					imports: ['showToast', 'showDialog', 'showConfirmDialog', 'showNotify', 'showImagePreview'],
+				},
+				{
+					from: 'lodash-es',
+					imports: lodashImports,
+				},
+			],
 			dts: 'src/auto-imports.d.ts',
 		}),
 		Components({
