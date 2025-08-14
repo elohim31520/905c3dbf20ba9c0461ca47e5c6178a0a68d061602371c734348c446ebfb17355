0c61a3502676e3f6650532e76bc7df18a293549a
lewis <versus7063@gmail.com>
2025-08-14 17:48:48 +0800

    feat: 新增跳轉路由每次都scroll至頂

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,10 @@ import { isAuthenticated } from '@/modules/auth'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
+	scrollBehavior(to, from, savedPosition) {
+		// always scroll to top
+		return { top: 0, behavior: 'smooth' }
+	},
 	routes: [
 		{
 			path: '/',
