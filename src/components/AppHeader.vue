5524c6ff2b7bafb7fc1ce3d78086990f527ff1e7
lewis <versus7063@gmail.com>
2025-08-20 18:36:42 +0800

    feat: 新增app logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
 	<header class="w-100% flex-y-center bg-white h-60 shadow-primary">
-		<div class="font-bold text-24 ml-10 color-primary">UrTrade</div>
+		<div class="w-100 h-100% bg-[url('/logo.png')] bg-no-repeat bg-center bg-contain scale-up ml-10"></div>
 		<div @click.prevent="$emit('update:showMenu', true)" class="p-2 ml-auto mr-10">
 			<van-icon name="wap-nav" size="24" />
 		</div>
@@ -8,14 +8,19 @@
 </template>
 
 <script lang="ts" setup>
-import VanIcon from 'vant/lib/icon'
-import 'vant/lib/icon/style'
+	import VanIcon from 'vant/lib/icon'
 
-const props = defineProps<{
-	showMenu: boolean
-}>()
+	const props = defineProps<{
+		showMenu: boolean
+	}>()
 
-const emit = defineEmits<{
-	(e: 'update:showMenu', value: boolean): void
-}>()
+	const emit = defineEmits<{
+		(e: 'update:showMenu', value: boolean): void
+	}>()
 </script>
+
+<style scoped>
+	.scale-up {
+		background-size: 130%;
+	}
+</style>
