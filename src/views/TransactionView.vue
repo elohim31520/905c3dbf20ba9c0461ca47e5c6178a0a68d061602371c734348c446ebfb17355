365e71a5ec81303db98fe5d26004d3ea36da62d2
lewis <versus7063@gmail.com>
2025-08-26 16:44:59 +0800

    update: 美化新增交易頁面的樣式

--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -1,64 +1,7 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import { showToast } from 'vant'
-import type { FormInstance } from 'vant'
-import { transactionApi } from '../api/transaction'
-import emitter from '@/modules/emitter'
-
-interface TransactionForm {
-	stock_id: string
-	transaction_type: 'buy' | 'sell'
-	quantity: string
-	price: string
-	transaction_date: string
-}
-
-const getInitialFormState = (): TransactionForm => ({
-	stock_id: '',
-	transaction_type: 'buy',
-	quantity: '',
-	price: '',
-	transaction_date: new Date().toISOString().split('T')[0],
-})
-
-const formRef = ref<FormInstance>()
-const form = ref<TransactionForm>(getInitialFormState())
-
-const onSubmit = async () => {
-	if (!formRef.value) return
-
-	try {
-		await formRef.value?.validate()
-		const payload = {
-			...form.value,
-			quantity: Number(form.value.quantity),
-			price: Number(form.value.price),
-		}
-		await transactionApi.recordMyTransactions(payload)
-		showToast({
-			type: 'success',
-			message: '交易紀錄已儲存',
-		})
-
-		// 刷新交易紀錄和投資組合頁面
-		emitter.emit('refresh', 'records')
-		emitter.emit('refresh', 'portfolio')
-		form.value = getInitialFormState()
-		formRef.value?.resetValidation()
-	} catch (error) {
-		console.error(error)
-		showToast({
-			type: 'fail',
-			message: '請檢查輸入資料',
-		})
-	}
-}
-</script>
-
 <template>
-	<div class="w-full mx-auto py-6">
-		<van-form ref="formRef" @submit="onSubmit">
-			<van-cell-group inset>
+	<div class="w-full mx-auto pt-10">
+		<van-form ref="formRef" @submit="onSubmit" class="shadow-2xl">
+			<van-cell-group class="!rounded-xl overflow-hidden">
 				<van-field
 					v-model="form.stock_id"
 					name="stock_id"
@@ -70,8 +13,8 @@ const onSubmit = async () => {
 				<van-field name="transaction_type" label="交易類型">
 					<template #input>
 						<van-radio-group v-model="form.transaction_type" direction="horizontal">
-							<van-radio name="buy" checked-color="#f472b6"> <span class="color-green-600">買入</span></van-radio>
-							<van-radio name="sell" checked-color="#f472b6"> <span class="color-red-600">賣出</span></van-radio>
+							<van-radio name="buy" checked-color="#f472b6"><span class="color-green-600">買入</span></van-radio>
+							<van-radio name="sell" checked-color="#f472b6"><span class="color-red-600">賣出</span></van-radio>
 						</van-radio-group>
 					</template>
 				</van-field>
@@ -109,10 +52,66 @@ const onSubmit = async () => {
 					:rules="[{ required: true, message: '請選擇交易日期' }]"
 				/>
 			</van-cell-group>
-
-			<div class="px-10 py-20">
-				<van-button round block type="primary" color="#f472b6" native-type="submit"> 紀錄 </van-button>
-			</div>
 		</van-form>
+		<div class="px-10 py-20">
+			<van-button round block type="primary" color="#f472b6" native-type="submit">紀錄</van-button>
+		</div>
 	</div>
 </template>
+
+<script setup lang="ts">
+	import { ref } from 'vue'
+	import { showToast } from 'vant'
+	import type { FormInstance } from 'vant'
+	import { transactionApi } from '../api/transaction'
+	import emitter from '@/modules/emitter'
+
+	interface TransactionForm {
+		stock_id: string
+		transaction_type: 'buy' | 'sell'
+		quantity: string
+		price: string
+		transaction_date: string
+	}
+
+	const getInitialFormState = (): TransactionForm => ({
+		stock_id: '',
+		transaction_type: 'buy',
+		quantity: '',
+		price: '',
+		transaction_date: new Date().toISOString().split('T')[0],
+	})
+
+	const formRef = ref<FormInstance>()
+	const form = ref<TransactionForm>(getInitialFormState())
+
+	const onSubmit = async () => {
+		if (!formRef.value) return
+
+		try {
+			await formRef.value?.validate()
+			const payload = {
+				...form.value,
+				quantity: Number(form.value.quantity),
+				price: Number(form.value.price),
+			}
+			await transactionApi.recordMyTransactions(payload)
+			showToast({
+				type: 'success',
+				message: '交易紀錄已儲存',
+			})
+
+			// 刷新交易紀錄和投資組合頁面
+			emitter.emit('refresh', 'records')
+			emitter.emit('refresh', 'portfolio')
+			form.value = getInitialFormState()
+			formRef.value?.resetValidation()
+		} catch (error) {
+			console.error(error)
+			showToast({
+				type: 'fail',
+				message: '請檢查輸入資料',
+			})
+		}
+	}
+</script>
