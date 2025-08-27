05c68804025446317024bf7a09866d32ea09b97e
lewis <versus7063@gmail.com>
2025-08-27 16:17:36 +0800

    update: 在companies頁面新增導航至詳情的導航方法

--- src/views/Companies.vue
+++ src/views/Companies.vue
@@ -2,7 +2,7 @@
 	<div class="bg-white p-10 shadow-primary">
 		<div class="text-20 font-bold">公司列表</div>
 		<div v-for="(company, index) in companies" :key="index" class="flex-y-center justify-between">
-			<div class="w-100% flex-y-center color-#434343 h-60 px-5 shadow-primary gap-5">
+			<div class="w-100% flex-y-center color-#434343 h-60 px-5 shadow-primary gap-5" @click="handleClick(company.symbol)">
 				<CompanyIcon :symbol="company.symbol" />
 				<div>{{ company.symbol }}</div>
 				<div class="ml-auto">{{ company.name }}</div>
@@ -15,13 +15,19 @@
 	import { ref, onMounted } from 'vue'
 	import { marketApi } from '../api/market'
 	import type { Company } from '../types/api'
-	import CompanyIcon from '../components/CompanyIcon.vue'
+	import { useRouter } from 'vue-router'
+
+	const router = useRouter()
 
 	defineOptions({
 		name: 'companies',
 	})
 
 	const companies = ref<Company[]>([])
+
+	const handleClick = (symbol: string) => {
+		router.push(`/company-metrics/${symbol}`)
+	}
 
 	onMounted(async () => {
 		const response = await marketApi.getStockSymbols()
