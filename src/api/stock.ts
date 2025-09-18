caf258c43d89e25f52835666b56708a19fe2585e
lewis <versus7063@gmail.com>
2025-09-18 18:34:16 +0800

    refactor:註解無用的getStockSymbols api

--- src/api/stock.ts
+++ src/api/stock.ts
@@ -2,6 +2,15 @@ import { httpClient } from '../modules/service'
 import type { ResponseData, Company } from '../types/api'
 
 class StockApi {
+	// 獲取公司列表改到前端的 /public ，因為雲端有出站費用很貴！
+	// async getStockSymbols(): Promise<ResponseData<Company[]>> {
+	// 	return httpClient.request<Company[]>({ method: 'GET', endpoint: '/stock/symbols' })
+	// }
+
+	// async getAllCompanies(): Promise<ResponseData<Company[]>> {
+	// 	return httpClient.request<Company[]>({ method: 'GET', endpoint: '/stock/symbols' })
+	// }
+
 	async getMarketBreadth(): Promise<ResponseData<number>> {
 		return httpClient.request<number>({ method: 'GET', endpoint: '/stock/breadth' })
 	}
