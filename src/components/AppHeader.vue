1a3f3f795ce5be88b6e909166715e9e1abc0c85e
lewis <versus7063@gmail.com>
2025-09-22 11:50:30 +0800

    feat: 點擊logo回首頁

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,9 @@
 <template>
 	<header class="w-100% flex-y-center bg-white h-60 shadow-primary">
-		<div class="w-100 h-100% bg-[url('/logo.webp')] bg-no-repeat bg-center bg-contain scale-up ml-10"></div>
+		<div
+			class="w-100 h-100% bg-[url('/logo.webp')] bg-no-repeat bg-center bg-contain scale-up ml-10"
+			@click="router.push('/')"
+		></div>
 		<div @click.prevent="uiStore.showMenu" class="p-2 ml-auto mr-10">
 			<van-icon name="wap-nav" size="24" />
 		</div>
@@ -10,8 +13,10 @@
 <script lang="ts" setup>
 	import VanIcon from 'vant/lib/icon'
 	import { useUIStore } from '@/stores/ui'
+	import { useRouter } from 'vue-router'
 
 	const uiStore = useUIStore()
+	const router = useRouter()
 </script>
 
 <style scoped>
