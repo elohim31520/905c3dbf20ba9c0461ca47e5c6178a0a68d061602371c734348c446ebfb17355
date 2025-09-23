d1e011683e1db380182c2fba26c9b1946e911d3a
lewis <versus7063@gmail.com>
2025-09-23 16:25:18 +0800

    update: 調整首頁組件的展示順序

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <template>
 	<main class="p-4 space-y-4">
-		<MarketBreadth />
+		<MomentumChart />
 		<HeatMap class="h-500 my-10" />
+		<MarketBreadth />
 		<VolatileAsset />
 		<SeeMore :to="'/volatile-stock'" class="my-10" />
-		<MomentumChart />
-
 		<!-- 公司入口 -->
 		<HotCompanies />
 		<SeeMore :to="'/m7'" class="my-10" />
