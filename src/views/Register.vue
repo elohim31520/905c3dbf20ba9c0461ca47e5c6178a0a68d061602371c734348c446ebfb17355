abc845c3b71416943eb7703a2d01be4674ca8da1
lewis <versus7063@gmail.com>
2025-09-24 11:56:24 +0800

    feat: 新增多語系配置

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,70 +1,74 @@
 <template>
 	<div>
-		<van-nav-bar title="註冊" />
+		<van-nav-bar :title="$t('register.title')" />
 		<van-form @submit="onSubmit">
 			<van-cell-group inset>
 				<van-field
 					v-model="username"
 					name="username"
-					label="帳號"
-					placeholder="請輸入帳號"
-					:rules="[{ required: true, message: '請填寫帳號' }]"
+					:label="$t('register.account')"
+					:placeholder="$t('register.enter_account')"
+					:rules="[{ required: true, message: $t('register.fill_account') }]"
 				/>
 				<van-field
 					v-model="email"
 					name="email"
-					label="電子郵件"
-					placeholder="請輸入電子郵件"
-					:rules="[{ required: true, message: '請填寫電子郵件' }]"
+					:label="$t('register.email')"
+					:placeholder="$t('register.enter_email')"
+					:rules="[{ required: true, message: $t('register.fill_email') }]"
 				/>
 				<van-field
 					v-model="password"
 					type="password"
 					name="password"
-					label="密碼"
-					placeholder="請輸入密碼"
-					:rules="[{ required: true, message: '請填寫密碼' }]"
+					:label="$t('register.password')"
+					:placeholder="$t('register.enter_password')"
+					:rules="[{ required: true, message: $t('register.fill_password') }]"
 				/>
 				<van-field
 					v-model="confirmPassword"
 					type="password"
 					name="confirmPassword"
-					label="確認密碼"
-					placeholder="請再次輸入密碼"
-					:rules="[{ validator: passwordValidator, message: '兩次輸入的密碼不一致' }]"
+					:label="$t('register.confirm_password')"
+					:placeholder="$t('register.enter_confirm_password')"
+					:rules="[{ validator: passwordValidator, message: $t('register.password_mismatch') }]"
 				/>
 			</van-cell-group>
 			<div style="margin: 16px">
-				<van-button round block type="primary" color="#f472b6" native-type="submit"> 註冊 </van-button>
+				<van-button round block type="primary" color="#f472b6" native-type="submit">
+					{{ $t('register.register') }}
+				</van-button>
 			</div>
 		</van-form>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { register } from '../api/user'
+	import { ref } from 'vue'
+	import { useRouter } from 'vue-router'
+	import { register } from '../api/user'
+	import { useI18n } from 'vue-i18n'
 
-const router = useRouter()
-const username = ref('')
-const email = ref('')
-const password = ref('')
-const confirmPassword = ref('')
+	const { t } = useI18n()
+	const router = useRouter()
+	const username = ref('')
+	const email = ref('')
+	const password = ref('')
+	const confirmPassword = ref('')
 
-const passwordValidator = (val: string) => {
-	return val === password.value
-}
-
-const onSubmit = async (values: any) => {
-	const res = await register({
-		name: values.username,
-		pwd: values.password,
-		email: values.email,
-	})
-	if (res.success) {
-		showToast('註冊成功')
-		router.push('/')
+	const passwordValidator = (val: string) => {
+		return val === password.value
 	}
-}
+
+	const onSubmit = async (values: any) => {
+		const res = await register({
+			name: values.username,
+			pwd: values.password,
+			email: values.email,
+		})
+		if (res.success) {
+			showToast(t('register.register_success'))
+			router.push('/')
+		}
+	}
 </script>
